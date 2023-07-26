0e49b7b5ef5a9f50fd7e92dff4f3b21fa9a6bf12
ko2 <rheorjs789@naver.com>
2023-07-27 01:14:32 +0900

    rails에 도메인 호스팅

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,9 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # Add your domain to the hosts configuration
+  config.hosts << "my.ko2135.shop"
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
